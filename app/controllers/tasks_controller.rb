0ecd01646645dde45dad328976c6fc5d8b79ddc5
sugimoto519 <rqsnq531@gmail.com>
2025-06-21 07:39:16 +0900

    today/weekｱｸｼｮﾝの実装・ﾋﾞｭｰの修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,20 @@
 class TasksController < ApplicationController
+  before_action :set_week, only: [:edit, :update, :destroy]
+
   def index
-    @tasks = Task.order("created_at ASC")
+    @tasks = current_user.tasks.order("created_at ASC")
+  end
+
+  def today 
+    @tasks = current_user.tasks.where(deadline: Time.zone.now.all_day).order(deadline: :asc)
+    @title = "今日のタスク"
+    render :index
+  end
+
+  def week
+    @tasks = current_user.tasks.where(deadline: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week).order(deadline: :asc)
+    @title = "今週のタスク"
+    render :index
   end
 
   def new
@@ -17,22 +31,23 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update(task_params)
+    @task.update(task_params)
     redirect_to root_path
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy 
+    @task.destroy 
     redirect_to root_path
   end
 
   private
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end 
 
   def task_params
     params.require(:task).permit(:task_name, :deadline, :priority_id, :status_id, :content).merge(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'tasks/index'
   root to: "tasks#index"
   devise_for :users
-  resources :tasks
-
+  resources :tasks do 
+    collection do 
+      get 'today'
+      get 'week'
+    end
+  end
 end
