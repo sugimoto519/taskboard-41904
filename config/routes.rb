74d42a1dab23c1dd1d5235fdac91bfa9d9ef4502
sugimoto519 <rqsnq531@gmail.com>
2025-06-22 08:10:56 +0900

    showアクションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   devise_for :users
   resources :tasks do 
+    resources :comments, only: [:create, :destroy]
     member do 
       patch :toggle_completion
     end 
