f10650e02b5cdc365c81759fd440eafbd991c0d7
sugimoto519 <rqsnq531@gmail.com>
2025-06-22 11:56:58 +0900

    コメントの編集/削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :update, :destroy]
+
   def create
     @task = Task.find(params[:task_id])
     @comment = @task.comments.build(comment_params)
@@ -12,13 +15,34 @@ class CommentsController < ApplicationController
     end 
   end
 
+  def edit
+    @task = @comment.task
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to task_path(@comment.task), notice: 'コメントを更新しました！'
+    else
+      @task = @comment.task
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def destroy
-    @comment = current_user.comments.find(params[:id])
+    task = @comment.task
     @comment.destroy
-    redirect_to task_path(@comment.task), notice: 'コメントを削除しました！', status: :see_other
+    redirect_to task_path(task), notice: 'コメントを削除しました！', status: :see_other
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_to task_path(@comment.task), alert: '権限がありません！' unless @comment.user == current_user
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   devise_for :users
   resources :tasks do 
-    resources :comments, only: [:create, :destroy]
+    resources :comments, only: [:create, :edit, :update, :destroy]
     member do 
       patch :toggle_completion
     end 
